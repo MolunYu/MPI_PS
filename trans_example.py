46a6c1d91fc3d2289e5145e93835adf21f54ae61
Molun Yu <molunyu97@163.com>
2019-11-28 13:46:56 +0800

    version 1.1

--- trans_example.py
+++ trans_example.py
@@ -2,10 +2,10 @@ from mpi4py import MPI
 import torch
 import torch.nn as nn
 import numpy as np
-import json
 import pickle
-from collections import OrderedDict
+import sys
 import matplotlib.pyplot as plt
+from collections import OrderedDict
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -25,6 +25,11 @@ x_train = np.array([[3.3], [4.4], [5.5], [6.71], [6.93], [4.168],
 y_train = np.array([[1.7], [2.76], [2.09], [3.19], [1.694], [1.573],
                     [3.366], [2.596], [2.53], [1.221], [2.827],
                     [3.465], [1.65], [2.904], [1.3]], dtype=np.float32)
+
+model = nn.Linear(input_size, output_size)
+grads = {name: param.grad for name, param in model.named_parameters()}
+grads_size = sys.getsizeof(pickle.dumps(grads))
+state_dict_size = sys.getsizeof(pickle.dumps(model.state_dict()))
 
 if rank == 0:
     model = nn.Linear(input_size, output_size)
@@ -47,24 +52,27 @@ if rank == 0:
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        grads = {name: param.grad for name, param in model.named_parameters()}
 
-        comm.Send(pickle.dumps(model.state_dict()), dest=1, tag=200)
+        comm.Send(pickle.dumps(grads), dest=1, tag=200)
 
+        data = bytearray(10000)
+        comm.Recv(data, source=1, tag=300)
+        remote_state_dict = OrderedDict(pickle.loads(data))
+        model.load_state_dict(remote_state_dict)
 
 else:
     model = nn.Linear(input_size, output_size)
-    criterion = nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-    data = bytearray(800)
-    comm.Recv(data, source=0, tag=200)
-    remote_state_dict = OrderedDict(pickle.loads(data))
-    local_state_dict = model.state_dict()
+    for epoch in range(num_epochs):
+        data = bytearray(10000)
+        comm.Recv(data, source=0, tag=200)
+        remote_grads = dict(pickle.loads(data))
 
-    for key in remote_state_dict:
-        local_state_dict[key] += remote_state_dict[key]
-        local_state_dict[key] /= 2
+        optimizer.zero_grad()
+        for name, param in model.named_parameters():
+            param.grad = remote_grads[name]
+        optimizer.step()
 
-    model.load_state_dict(local_state_dict)
-
-
+        comm.Send(pickle.dumps(model.state_dict()), dest=0, tag=300)
